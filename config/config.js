cf0daf54f05512933176a9aa64faa3c5b8bfdd4c
Aldev <donialpian123456@gmail.com>
2025-01-14 10:56:40 +0700

    Create config.js

---
+++ config/config.js
@@ -0,0 +1,14 @@
+const config = {
+  author: "Shannz",
+  messages: {
+    query: "Parameter Query Belum Diisi!!",
+    url: "Parameter Url Belum Diisi!!",
+    prompt: "Parameter Prompt Belum Diisi!!",
+    id: "Parameter Id Belum Diisi!!",
+    error: "Internal Server Error!",
+    key: "Apikey diperlukan, kunjungi dashboard untuk selengkapnya.",
+    limit: "Limit mu sudah habis, cobalah membeli limit baru!",
+  }
+};
+
+module.exports = config;
